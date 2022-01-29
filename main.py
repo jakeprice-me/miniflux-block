068ace74a10a838f631198cbb1a2109b5d92bd3f
Jake Price <git@jakeprice.dev>
2022-01-29 15:39:36 +0000

    Fix pylint issues and add CA
    - Fix a number of pylint issues
    - Add path to custom certificate authority so I can stop ignoring SSL
      validation errors.

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
+"""
+Mark Miniflux RSS entries as read if they match word/phrase in blocklist.
+"""
+
 import re
 import logging
 import json
 import requests
 import yaml
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def main():
 
+    """
+    Main functionality
+    """
+
     # Configuration file:
-    with open("config.yml", "r") as config:
+    with open("config.yml", "r", encoding="utf-8") as config:
 
         # Load config file:
         config_file = yaml.safe_load(config)
 
-        # Get config values:
-        miniflux_api_token = config_file["miniflux_api_token"]
-        miniflux_base_url = config_file["miniflux_base_url"]
-        blocklist = config_file["blocklist"]
-
     # API Configuration:
-    auth_token = miniflux_api_token
-    base_url = miniflux_base_url
+    auth_token = config_file["miniflux_api_token"]
+    base_url = config_file["miniflux_base_url"]
     api_url = "/v1"
     api_entries = "/entries"
-    api_entries_params = {"status": "unread", "order": "published_at", "direction": "asc"}
+    api_entries_params = {
+        "status": "unread",
+        "order": "published_at",
+        "direction": "asc",
+    }
+    custom_ca_path=config_file["custom_ca_path"]
     api_endpoint = base_url + api_url + api_entries
 
     # API call:
     entries = requests.get(
         api_endpoint,
         params=api_entries_params,
-        verify=False,
+        verify=custom_ca_path,
         headers={"X-Auth-Token": auth_token},
     )
 
-    entries_list = entries.json()["entries"]
-
     # Load config file:
-    for entry in entries_list:
+    for entry in entries.json()["entries"]:
 
         # Entry values:
         entry_title = entry["title"]
@@ -47,24 +51,18 @@ def main():
         entry_published_at = entry["published_at"]
         entry_feed_title = entry["feed"]["title"]
 
-        for word in blocklist:
-
-            # Regex to check:
-            blocklist_regex = rf"({word})"
+        for word in config_file["blocklist"]:
 
             # Check against title only:
-            blocklist_match = re.search(blocklist_regex, entry_title, re.IGNORECASE)
+            blocklist_match = re.search(rf"({word})", entry_title, re.IGNORECASE)
 
             if blocklist_match:
 
-                # Set entry status to read:
-                api_entries_json = {"entry_ids": [entry_id], "status": "read"}
-
                 # Mark matches entries as read:
-                read_entries = requests.put(
+                requests.put(
                     api_endpoint,
-                    data=json.dumps(api_entries_json),
-                    verify=False,
+                    data=json.dumps({"entry_ids": [entry_id], "status": "read"}),
+                    verify=custom_ca_path,
                     headers={
                         "X-Auth-Token": auth_token,
                         "Content-Type": "application/json",
@@ -75,12 +73,23 @@ def main():
                 logging.basicConfig(
                     level=logging.INFO,
                     format="%(asctime)s - [%(levelname)s] - %(message)s",
-                    handlers=[logging.FileHandler("blocked.log"), logging.StreamHandler()],
+                    handlers=[
+                        logging.FileHandler("blocked.log"),
+                        logging.StreamHandler(),
+                    ],
                     datefmt="%Y-%m-%d %H:%M:%S",
                 )
 
                 # Log output:
-                logging.info(f"{entry_id} - {entry_published_at} - {entry_feed_title} - Blocked: {word} - {entry_title}")
+                logging.info(
+                    "%s - %s - %s - %s - %s",
+                    entry_id,
+                    entry_published_at,
+                    entry_feed_title,
+                    word,
+                    entry_title,
+                )
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
